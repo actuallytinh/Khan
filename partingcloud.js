713effbd022d6191fa45099cc137eed0dbbd19f1
Tinh Truong <62217004+actuallytinh@users.noreply.github.com>
2020-10-19 11:04:53 -0400

    Added Script

---
+++ partingcloud.js
@@ -0,0 +1,25 @@
+noStroke();
+var leftX = 122;
+var rightX = 274;
+var sunRadius = 100;
+
+draw = function() {
+    background(184, 236, 255);
+    
+    fill(255, 170, 0);
+    ellipse(200, 100, sunRadius, sunRadius);
+    sunRadius+= 2;
+    // clouds 
+    fill(255, 255, 255);
+    // left cloud
+    ellipse(leftX, 150, 126, 97);
+    ellipse(leftX+62, 150, 70, 60);
+    ellipse(leftX-62, 150, 70, 60);
+    leftX-= 1;
+    
+    // right cloud
+    ellipse(rightX, 100, 126, 97);
+    ellipse(rightX+62, 100, 70, 60);
+    ellipse(rightX-62, 100, 70, 60);
+    rightX+= 1;
+};
