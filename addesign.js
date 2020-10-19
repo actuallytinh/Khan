7bf1f68049bac73657e027ab9a39f554adf31648
Tinh Truong <62217004+actuallytinh@users.noreply.github.com>
2020-10-19 11:06:04 -0400

    Added Script

---
+++ addesign.js
@@ -0,0 +1,14 @@
+fill(0, 0, 0);
+textSize(30);
+text("TOY ROCKET SHIP", 10, 30);
+textSize(19);
+text("Only $4.99", 223, 58);
+textSize(15);
+text("Fun for all ages!", 14, 58);
+
+noStroke();
+fill(0, 30, 128);
+rect(185,170,50,99);
+triangle(190,228,186,269,131,302);
+triangle(190,185,235,269,305,302);
+triangle(209, 128, 185, 171, 234 ,170);
