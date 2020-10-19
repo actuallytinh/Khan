5aeeee04c388021c1ec26868693188668791488f
Tinh Truong <62217004+actuallytinh@users.noreply.github.com>
2020-10-19 11:05:24 -0400

    Added Script

---
+++ favoritefoods.js
@@ -0,0 +1,6 @@
+fill(0, 0, 0);
+textSize(20);
+text("My Favorite Foods:", 31, 43);text("My Favorite Foods:", 31, 43);
+text("1. Sushi", 31, 70);
+text("2. Burgers", 31, 100);
+text("3. Fries", 31, 130);
